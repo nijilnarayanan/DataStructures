e0145cc42e6381eb114d48a4ea95329a1ef00cc0
Nijil Narayanan <nijil.narayanan@bookmyshow.com>
2018-11-16 19:17:41 +0530

    change in css

--- j.js
+++ j.js
@@ -1,8 +1,10 @@
 const array = [...Array(11).keys()]
 
+const regex = /(-?[0-9]+)\s*-{1}\s*(-?[0-9]+)|(-?[0-9]+)/gm;
+
 function validate(){
     
-    let inputValue = document.getElementById('input').value
+    let inputValue = document.getElementById('input').value.trim()
  
     console.log(inputValue)
 
@@ -21,15 +23,15 @@ function validate(){
             if(availableValues.length > 0){
                 availableValues.toString()  
                 console.log(availableValues)
-                alert(`${availableValues} is available.`)               
+                document.getElementById('output').innerHTML=  `${availableValues} is available.`
                 } else {
-                alert('Already, this number range is taken. Try other number')
+                document.getElementById('output').innerHTML= 'Already, this number range is taken. Try other number'
                 }
    } 
     else if(array.indexOf(Number(inputValue)) != -1) {  
-        alert('Already, this number is taken. Try other number')
+        document.getElementById('output').innerHTML= 'Already, this number is taken. Try other number'
     }
      else {
-         alert(`${inputValue} is available.`)   
+        document.getElementById('output').innerHTML= `${inputValue} is available.`
         }
 }
